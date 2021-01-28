92acee7e210ab9f3a6641a89c32b46a1f4a19adc
Saikrishna <mahendarkarsai7@gmail.com>
2021-01-29 01:45:18 +0530

    customCommands

--- cypress/integration/examples/coderbyte.test.spec.js
+++ cypress/integration/examples/coderbyte.test.spec.js
@@ -6,6 +6,7 @@ import intersectPage from '../pageObjects/intersectPage'
 describe('Coderbyte Test', function () {
 
     beforeEach(function () {
+        cy.cleanup()
         cy.fixture('data').then((data) => {
             this.data = data
         })
@@ -27,29 +28,6 @@ describe('Coderbyte Test', function () {
         cy.visit('/solution/intersect/')
         intersect.map().scrollIntoView({ duration: 2000 })
             .should('be.visible')
-
-        intersect.stateList().children('option').then(option => {
-            var actual = [...option].map(o => o.value)
-            actual.shift()
-            var popupText = [...option].map(o => o.dataset.popupText)
-            popupText.shift()
-
-            var iterator = actual.values()
-
-            for (let elements of iterator) {
-                var i = 0
-                elements = elements.substring(0, 2)
-                var tag = '[data-state="'
-                elements = tag.concat(elements).concat('"]')
-                cy.get(elements)
-                    .invoke('trigger', 'mouseover').click({ force: true }).invoke('show')
-                cy.wait(1000)
-                cy.get('.block-stats-map__popup-content').invoke('text').then((text) => {
-                    const Text = text
-                    expect(popupText[i]).eq(Text)
-                    i = i + 1
-                })
-            }
-        })
+        cy.statePopup()
     })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -10,7 +10,37 @@
 //
 //
 // -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
+Cypress.Commands.add('cleanup', () => {
+    cy.clearCookies();
+    cy.clearLocalStorage();
+    sessionStorage.clear();
+  });
+
+Cypress.Commands.add("statePopup", () => {
+    cy.get('.block-stats-map__state-dropdown').children('option').then(option => {
+        var actual = [...option].map(o => o.value)
+        actual.shift()
+        var popupText = [...option].map(o => o.dataset.popupText)
+        popupText.shift()
+
+        var iterator = actual.values()
+
+        for (let elements of iterator) {
+            var i = 0
+            elements = elements.substring(0, 2)
+            var tag = '[data-state="'
+            elements = tag.concat(elements).concat('"]')
+            cy.get(elements)
+                .invoke('trigger', 'mouseover').click({ force: true }).invoke('show')
+            cy.wait(1000)
+            cy.get('.block-stats-map__popup-content').invoke('text').then((text) => {
+                const Text = text
+                expect(popupText[i]).eq(Text)
+                i = i + 1
+            })
+        }
+    })
+})
 //
 //
 // -- This is a child command --
